f61faf8a4fad0e727f724ace6502a9402a972123
Kennedy Rose <kennedyianrose@gmail.com>
2020-03-25 15:59:17 -0500

    Minor revisions

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,11 @@
+require(`dotenv`).config({ silent: true })
+
 module.exports = {
   plugins: [
     {
       resolve: `gatsby-theme-service-relief`,
       options: {
-        authorName: `SmarterLabs_`,
+        authorName: `Smarter Labs`,
         authorLink: `https://smarterlabs.com/`,
       },
     },
