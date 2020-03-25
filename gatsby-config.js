91c50769e21acfe31b44181c9e0d97018ea4379b
Kennedy Rose <kennedyianrose@gmail.com>
2020-03-25 14:32:01 -0500

    Added Labs link

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       resolve: `gatsby-theme-service-relief`,
       options: {
-        authorName: `some Gatsby folks`,
-        authorLink: `https://twitter.com/gatsbyjs`,
+        authorName: `SmarterLabs_`,
+        authorLink: `https://smarterlabs.com/`,
       },
     },
   ],
